68a6a8c2c3f09a9bc4c3badd539942269572e24e
emilra <emily.raine@live.co.uk>
2019-12-10 22:36:22 +0000

    Setup project structure

---
+++ RobotCrosswordSolver/RobotCrosswordSolverTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RobotCrosswordSolverTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
